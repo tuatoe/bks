c78bb6c4caab1bed5ff59bc07b1a9bdf47555d38
Tuatoe Daynuah <tuatoedaynuah@Tuatoes-iMac-2.hsd1.mn.comcast.net>
2022-06-10 08:09:54 -0500

    nav menu

---
+++ src/components/nav-desktop/NavDesktop.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function NavDesktop() {
+
+    return (
+        <div className='nav-desktop'>
+            <ul>
+                <li><a className="link is-active" href="">About us</a></li>
+                <li><a className="link" href="">What we do</a></li>
+                <li><a className="link" href="">How you can help</a></li>
+                <li><a className="link" href="">Eevents</a></li>
+                <li><a className="link" href="">Gallery</a></li>
+                <li><a className="link" href="">Contact</a></li>
+                <li><a className="link" href="">Donate</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavDesktop;
--- src/header/index.js
+++ src/header/index.js
@@ -1,45 +1,43 @@
-import React, { useState, useRef, useEffect } from 'react'
-import Nav from '../components/nav'
+import React, { useState, useRef } from 'react';
+import NavMobile from '../components/nav-mobile'
+import NavDesktop from '../components/nav-desktop/NavDesktop'
 import logo from './bks_logo.png'
 
 function Header() {
     const headerRef = useRef()
     const [isOpen, setMenu] = useState(false)
-    const [status, setStatus] = useState(false)
+
     const toggle = e => {
         e.stopPropagation();
         setMenu(!isOpen)
-        setStatus(!isOpen)
     }
 
     return (
-        <header className="heading container menubar" ref={headerRef}>
-            <div className="nav flex" >
-                <img className="logo" src={logo} alt='Benjamin Karmon Sehkar Foundation' tabIndex='1' />
-                <Nav status={status} onClick={toggle} />
-                <div className='flex__item'>
-                    <button
-                        onClick={toggle}
-                        id='hambmenu'
-                        className={status}
-                    >
-                        <span />
-                        <span />
-                        <span />
-                        <span />
-                    </button>
+        <header className='heading container menubar' ref={headerRef}>
+            <div className='nav flex' >
+                <img className='logo' src={logo} alt='Benjamin Karmon Sehkar Foundation' tabIndex='1' />
+                <div className='nav-mobile'>
+                    <NavMobile status={isOpen ? 'isopen' : ''} onClick={toggle} />
+                    <div className='flex__item'>
+                        <button
+                            onClick={toggle}
+                            id='hambmenu'
+                            className={isOpen ? 'isopen' : ''}
+                        >
+                            <span />
+                            <span />
+                            <span />
+                            <span />
+                        </button>
+                    </div>
                 </div>
+                <NavDesktop />
             </div>
             <div className='hero'>
                 <h2 className='hero__title-one'>Equal Access to Quality Eduation</h2>
                 <h3 className='hero__title-two'>Empowerment Through Education</h3>
-                <a className='link' href="#">DONATE</a>
+                <a className='link' href='#'>DONATE</a>
             </div>
-            {/* <span>
-            {windowWidth <= targetWidth && (<NavMobile />)}
-            {!windowWidth <= targetWidth && (<NavDesktop />)}
-          </span> */}
-
         </header>
     )
 }
